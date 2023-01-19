feaa4a3cbf8e7df9e0019e413c9da5535d12b593
DiyanKalaydzhiev23 <diankostadenov@gmail.com>
2023-01-19 19:18:04 +0200

    Added more solutions

--- stacks_queues_and_tuples_exercise/milkshakes.py
+++ stacks_queues_and_tuples_exercise/milkshakes.py
@@ -1,33 +1,33 @@
 from collections import deque
-#rename it
-chocolates = list(int(x) for x in input().split(", "))
+
+chocolates = deque(int(x) for x in input().split(", "))
 cups_of_milk = deque(int(x) for x in input().split(", "))
 
-chocolate_milkshakes = 0
+milkshakes = 0
 
-while chocolate_milkshakes != 5 and chocolates and cups_of_milk:
+while milkshakes != 5 and cups_of_milk and chocolates:
     chocolate = chocolates.pop()
     cup_of_milk = cups_of_milk.popleft()
 
-    if chocolate < 1 and cup_of_milk < 1:
+    if chocolate <= 0 and cup_of_milk <= 0:
         continue
-    elif chocolate < 1:
+    elif chocolate <= 0:
         cups_of_milk.appendleft(cup_of_milk)
         continue
-    elif cup_of_milk < 1:
+    elif cup_of_milk <= 0:
         chocolates.append(chocolate)
         continue
 
     if chocolate == cup_of_milk:
-        chocolate_milkshakes += 1
+        milkshakes += 1
     else:
         cups_of_milk.append(cup_of_milk)
-        chocolate -= 5
-        chocolates.append(chocolate)
+        chocolates.append(chocolate - 5)
 
+if milkshakes == 5:
+    print("Great! You made all the chocolate milkshakes needed!")
+else:
+    print("Not enough milkshakes.")
 
-print('Great! You made all the chocolate milkshakes needed!') \
-    if chocolate_milkshakes == 5 else print('Not enough milkshakes.')
-print(f'Chocolate: {", ".join(str(x) for x in chocolates) if chocolates else "empty"}')
-print(f'Milk: {", ".join(str(x) for x in cups_of_milk) if cups_of_milk else "empty"}')
-
+print(f"Chocolate: {', '.join(str(x) for x in chocolates) or 'empty'}")
+print(f"Milk: {', '.join(str(x) for x in cups_of_milk) or 'empty'}")
